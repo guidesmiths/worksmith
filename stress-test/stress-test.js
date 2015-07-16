62c381bfc6dae768c2e6283d77d87ad2a8c03ae2
feliun <felipe.polo@gmail.com>
2015-07-17 00:12:02 +0100

    Improving the average function reusability for stress tests

--- stress-test/stress-test.js
+++ stress-test/stress-test.js
@@ -11,14 +11,14 @@ try {
     worksmith.use("lodash", worksmith.createAdapter(require('lodash')))
     var workflow = worksmith("./stress-test/stress-workflows/workflow1.js")
     console.log('About to execute workflow1')
-    calculateAverageExecTime(function(average) {
+    calculateAverageExecTime(workflow, function(average) {
         console.log('The average execution time is: ', average, ' ms')
     })
 } catch(e) {
     console.error('There was an error setting up a workflow: ', e)
 }
 
-function calculateAverageExecTime(next) {
+function calculateAverageExecTime(workflow, next) {
     var average = counter = 0
     async.whilst(
         function () { return counter < SAMPLE_SIZE },
@@ -26,12 +26,13 @@ function calculateAverageExecTime(next) {
             counter++;
             var ctx = {}
             workflow(ctx, function(err, results) {
-                if (err) console.error('There was an error running a workflow: ', err.toString())
+                if (err) return cb(err)
                 average += ctx.duration
                 cb()
             })
         },
         function (err) {
+            if (err) console.error('There was an error running a workflow: ', err.toString())
             next(average / SAMPLE_SIZE)
         }
     )
